534545f3b93fd8150b17ea1dc40330446d8d18ac
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-11-01 23:43:19 +0330

    define constants

--- src/utils/constants/constants.ts
+++ src/utils/constants/constants.ts
@@ -1,1 +1,4 @@
-export const API_ADDRESS = 'http://localhost:5000/api';
+export const BASE_API_ADDRESS = 'https://europe-west1-quickstart-1573558070219.cloudfunctions.net';
+export const PROXY_URL = 'https://cors-anywhere.herokuapp.com';
+export const BATCH_URL = '/file-batch-api';
+export const PROXIFIED_API_ADDRESS = `${PROXY_URL}/${BASE_API_ADDRESS}`;
