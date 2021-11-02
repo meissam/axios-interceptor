b45c26ccb795dd5479e9036e44c9e78de63d84e5
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-11-02 11:08:09 +0330

    remove header

--- src/utils/helpers/apiClient.ts
+++ src/utils/helpers/apiClient.ts
@@ -5,9 +5,6 @@ export const client = () => {
     const config: AxiosRequestConfig = {
         url: BASE_API_ADDRESS,
         baseURL: BASE_API_ADDRESS,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-        },
     };
     const instance = axios.create(config);
     batchInterceptor(instance);
