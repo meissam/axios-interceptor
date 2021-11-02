672bb4f3c76731181ac2e977a470360d24297019
Meissam Rasouli <meissam.rasouli@gmail.com>
2021-11-02 11:06:07 +0330

    add allow origin

--- src/utils/helpers/apiClient.ts
+++ src/utils/helpers/apiClient.ts
@@ -5,7 +5,9 @@ export const client = () => {
     const config: AxiosRequestConfig = {
         url: BASE_API_ADDRESS,
         baseURL: BASE_API_ADDRESS,
-        headers: {},
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+        },
     };
     const instance = axios.create(config);
     batchInterceptor(instance);
